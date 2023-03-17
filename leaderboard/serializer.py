7eeff77259b7db8ddfb6b02023f0a3d8bcd5ac88
tes-b <tsyi0313@gmail.com>
2023-03-17 17:27:23 +0900

    edit: missing files

--- leaderboard/serializer.py
+++ leaderboard/serializer.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Record
 
 class RecordSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Record
         fields = '__all__'
+
+    

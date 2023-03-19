c2c3bea1c748a060ea11a40d7a79e3ba2a1a0f8e
tes-b <tsyi0313@gmail.com>
2023-03-20 00:02:56 +0900

    add: town: charactor

--- static/js/town.js
+++ static/js/town.js
@@ -1,12 +1,11 @@
 var canvas = document.getElementById('canvas');
+
+canvas.setAttribute("width", window.innerWidth);
+canvas.setAttribute("height", window.innerHeight);
+
 var ctx = canvas.getContext('2d');
+ctx.imageSmoothingEnabled = false; // 이미지 부드럽게 처리하지 않음
 
-canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight -100;
-
-// var imgCharactor = new Image();
-// // imgCharactor.src = imgCharactorSrc;
-// imgCharactor.src = "img/charactor/DinoSprites_doux.gif"
 
 class Charactor {
     constructor() {
@@ -17,28 +16,135 @@ class Charactor {
         this.widthHalf = 25;
         this.heightHalf = 25;
         this.jumpPower = 4;        
-        this.image = new Image();
-        // this.image.src = imgCharactorSrc;
-        this.image.src = imgCharactorSrc;
-        // this.image.onload = () => {
-        //     this.drawImage();
-        // };
+        this.isJumping = false;
+        this.jumpDuration = 2000;
+        this.jumpTimer = 0;
+        this.isMovingLeft = false;
+        this.isMovingRight = false;
+        this.isMovingUp = false;
+        this.isMovingDown = false;
+        this.moveSpeed = 5;
+
+        this.state = "idle"
+
+        this.imgDict = {};
+
+        this.imgIdle = new Image();
+        this.imgIdle.src = imgCharactorIdle;
+        this.imgRunning = new Image();
+        this.imgRunning.src = imgCharactorRunning;
+
+        this.imgDict["idle"] = {"obj":this.imgIdle, "frameX":2, "frameY":1 ,"frameWidth":32, "frameHeight":32, "frameRate":30};
+        this.imgDict["running"] = {"obj":this.imgRunning, "frameX":3, "frameY":1 ,"frameWidth":32, "frameHeight":32, "frameRate":10};
+
+        this.imgCurrent = this.imgDict[this.state];
+
+        this.imgFrameWidth = 32;
+        this.imgFrameHeight = 32;
+
+        this.currentFrameX = 0;
+        this.currentFrameY = 0;
+
+        this.frameCount = 0;
+        
+        this.scale = 4;        
+    }
+
+    changeState(state) {
+        this.state = state;
+        this.currentFrameX = 0;
+        this.currentFrameY = 0;
+        this.frameCount = 0;
+        this.imgCurrent = this.imgDict[this.state];
     }
 
     drawHitbox() {
-        ctx.fillStyle = 'green';
-        ctx.strokeRect(this.x - this.widthHalf,this.y - this.heightHalf, this.width,this.height);
+        // ctx.fillStyle = 'green';
+        ctx.strokeRect(this.x - this.widthHalf,this.y - this.heightHalf, this.width * this.scale,this.height * this.scale);
     }
-    drawImage() {
-        if(this.image.complete){
-            ctx.drawImage(this.image, this.x - this.widthHalf,this.y - this.heightHalf, this.width,this.height);
+
+    drawFrame(frameX, frameY) {
+        if(this.imgCurrent["obj"].complete){
+            ctx.drawImage(
+                this.imgCurrent["obj"],
+                frameX * this.imgCurrent["frameWidth"],
+                frameY * this.imgCurrent["frameHeight"],
+                this.imgCurrent["frameWidth"],
+                this.imgCurrent["frameHeight"],
+                this.x - this.widthHalf,
+                this.y - this.heightHalf, 
+                this.width * this.scale,
+                this.height * this.scale, 
+                );
         }
     }
+
+    drawImage() {
+        if(this.imgCurrent["obj"].complete){
+            ctx.drawImage(
+                this.imgCurrent["obj"], 
+                0,
+                0,
+                this.imgCurrent["frameWidth"],
+                this.imgCurrent["frameHeight"],
+                this.x - this.widthHalf,
+                this.y - this.heightHalf, 
+                this.width * this.scale,
+                this.height * this.scale, 
+                );
+        }
+    }
+
+    animateImage() {
+        this.drawFrame(this.currentFrameX,this.currentFrameY);
+        this.frameCount++;
+        
+        if (this.frameCount >= this.imgCurrent["frameRate"]){
+            this.currentFrameX++;
+            if (this.currentFrameX >= this.imgCurrent["frameX"]) {
+                this.currentFrameX = 0;
+            }
+            this.frameCount = 0;
+        }
+    }
+
     draw(hitbox) {
         if (hitbox) {
             this.drawHitbox();
         }
-        this.drawImage();
+        // this.drawImage();
+        // this.drawFrame(this.currentFrameX,this.currentFrameY);
+        this.animateImage();
+    }
+
+    move(elapsedTime) {
+        if (this.isJumping) {
+            this.y -= this.jumpPower;
+            this.jumpTimer += elapsedTime;
+        }
+
+        // gravity
+        // if (this.y < floorHeight){
+        //     this.y += gravity;
+        // }
+        // jump
+        if (this.jumpTimer > this.jumpDuration) {
+            this.isJumping = false;
+            this.jumpTimer = 0;
+        }
+        // move
+        if (this.isMovingLeft) {
+            this.x -= this.moveSpeed;
+        }
+        else if (this.isMovingRight) {
+            this.x += this.moveSpeed;
+        }
+        if (this.isMovingUp) {
+            this.y -= this.moveSpeed;
+        }
+        else if (this.isMovingDown) {
+            this.y += this.moveSpeed;
+        }
     }
 }
 
@@ -77,6 +183,53 @@ var animation;
 
 var charactor = new Charactor();
 
+function keyInput() {
+    document.addEventListener('keydown', function(e) {
+        if (e.code === 'Enter') {
+            if (charactor.state === "idle") {
+                charactor.changeState("running");
+            } else {
+                charactor.changeState("idle");
+            }
+            
+        }
+        if (e.code === 'Space') {
+            // if (!charactor.isJumping) {
+            //     charactor.isJumping = true;
+            // }
+        }
+        if (e.code === 'ArrowLeft') {
+            charactor.isMovingLeft = true;
+        }
+        if (e.code === 'ArrowRight') {
+            charactor.isMovingRight = true;
+        }
+        if (e.code === 'ArrowUp') {
+            charactor.isMovingUp = true;   
+        }
+        if (e.code === 'ArrowDown') {
+            charactor.isMovingDown = true;
+        }
+    })    
+
+    document.addEventListener('keyup', function(e) {
+        if (e.code === 'ArrowLeft') {
+            charactor.isMovingLeft = false;
+        }
+        if (e.code === 'ArrowRight') {
+            charactor.isMovingRight = false;
+        }
+        if (e.code === 'ArrowUp') {
+            charactor.isMovingUp = false;   
+        }
+        if (e.code === 'ArrowDown') {
+            charactor.isMovingDown = false;
+        }
+    })
+}
+
+keyInput();
+
 
 function run() {
     animation = requestAnimationFrame(run);
@@ -97,7 +250,7 @@ function run() {
                 cactus.x -= cactus.speed;
 
                 if(collisionCheck(charactor, cactus)) {
-                    cancelAnimationFrame(animation);
+                    // cancelAnimationFrame(animation);
                 }
 
                 cactus.draw();
@@ -106,19 +259,20 @@ function run() {
             return false;
         })
 
-        if (isJumping) {
-            charactor.y -= charactor.jumpPower;
-            jumpTimer += elapsedTime;
-        }
+        // if (charactor.isJumping) {
+        //     charactor.y -= charactor.jumpPower;
+        //     jumpTimer += elapsedTime;
+        // }
 
-        if (charactor.y < floorHeight){
-            charactor.y += gravity;
-        }
-        if (jumpTimer > jumpDuration) {
-            isJumping = false;
-            jumpTimer = 0;
-        }
-
+        // if (charactor.y < floorHeight){
+        //     charactor.y += gravity;
+        // }
+        // if (jumpTimer > jumpDuration) {
+        //     charactor.isJumping = false;
+        //     jumpTimer = 0;
+        // }
+        
+        charactor.move(elapsedTime);
         charactor.draw(true);
         
         //===================================================
@@ -145,14 +299,5 @@ function collisionCheck(obj1, obj2) {
     // if (diffR > 0 && )
 }
 
-var isJumping = false;
-var jumpTimer = 0;
-var jumpDuration = 2000;
 
-document.addEventListener('keydown', function(e) {
-    if (e.code === 'Space') {
-        if (!isJumping) {
-            isJumping = true;
-        }
-    }
-})
+

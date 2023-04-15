7ce1292a65ed849966420ef0896c20e19bc14ddf
tes-b <tsyi0313@gmail.com>
2023-04-15 12:28:06 +0900

    edit: settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,7 +37,6 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = True
 
 ALLOWED_HOSTS = []

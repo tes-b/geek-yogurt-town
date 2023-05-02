d638cb669d00eb36c701353e64b5cf1f6c09a69b
tes-b <tsyi0313@gmail.com>
2023-05-02 23:06:30 +0900

    edit: town/views

--- town/views.py
+++ town/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 def index(request): # 첫화면
     # return HttpResponse("game index")
     return render(request, 'town/index.html') # 게임 페이지로 이동
-
-def wordle(request): # 첫화면
-    return render(request, 'wordle/main.html') # 게임 페이지로 이동
